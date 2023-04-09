354009d067e20e7b505c721c86ac8c80afeb689e
Vladislav <vladsarana@gmail.com>
2023-04-09 20:35:40 +0600

    added support of retrofit

---
+++ lib/retrofit_repository/cat_fact_service.g.dart
@@ -0,0 +1,69 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cat_fact_service.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CatFact _$CatFactFromJson(Map<String, dynamic> json) => CatFact(
+      fact: json['fact'] as String?,
+    );
+
+Map<String, dynamic> _$CatFactToJson(CatFact instance) => <String, dynamic>{
+      'fact': instance.fact,
+    };
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _RestClient implements RestClient {
+  _RestClient(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://catfact.ninja/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<dynamic> getFact() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio.fetch(_setStreamType<dynamic>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/fact',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data;
+    return value['fact'];
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
